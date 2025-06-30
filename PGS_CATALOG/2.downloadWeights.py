950f89103b1d6011611630f7e98f4da7a6cb10ea
domInIk0x0 <dominikpietrzak123p@gmail.com>
2025-06-30 10:48:40 +0200

    Create 2.downloadWeights.py

---
+++ PGS_CATALOG/2.downloadWeights.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import time
+import os
+import subprocess
+from tqdm import tqdm
+import shutil
+
+
+df = pd.read_csv('scraped_table_PGSCATALOG.csv')
+df['Num SNPs'] = df['Num SNPs'].str.replace(',', '', regex=False).astype(int)
+filtered_df = df[df['Num SNPs'] > 2000]
+df_no_duplicates = filtered_df.drop_duplicates(subset='Trait', keep='first')
+
+links = df_no_duplicates['Download Link'].values
+traits = df_no_duplicates ['Trait'].values
+
+dir_path = '/mnt/ip114/DominikPietrzak/gene/pgs_weights/PGS_CATALOG_WEIGHTS/'
+folders = os.listdir(dir_path)
+
+for f in folders:
+  p = os.path.join(dir_path, f)
+  shutil.rmtree(p)
+print('Folder structure has been cleaned.')
+print('\n')
+
+for li, traits in tqdm(zip(links, traits), total=len(links)):
+    target_dir = os.path.join(dir_path, traits.replace(' ', '_').upper())
+    print(f'Path: {target_dir}')
+    os.makedirs(target_dir, exist_ok=True)
+
+    time.sleep(1)
+    subprocess.run(['wget', '-P', target_dir, li])
