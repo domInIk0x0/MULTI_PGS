92265805e18434d55b1319d919cdab9fc1f0598f
domInIk0x0 <dominikpietrzak123p@gmail.com>
2025-06-30 10:50:20 +0200

    Create 3.weightsPreparing.R

---
+++ PGS_CATALOG/3.weightsPreparing.R
@@ -0,0 +1,14 @@
+library(data.table)
+library(readr)
+
+folders <- list.dirs('/mnt/ip114/DominikPietrzak/gene/pgs_weights/PGS_CATALOG_WEIGHTS/', recursive = FALSE)
+
+for (f in folders) {
+  file <- file.path(f, list.files(f)[1])
+  print(file)
+  weights_path <- file.path(f, 'weights.txt')
+  weights_clean_path <- file.path(f, 'weights_clean.txt')
+
+  system(paste('zcat', shQuote(file), '>', shQuote(weights_path)))
+  system(paste('sed "/^#/d" ', shQuote(weights_path), '>', shQuote(weights_clean_path)))
+}
